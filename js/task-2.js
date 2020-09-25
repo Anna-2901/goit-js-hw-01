84222d6f7300d7d719f1418d5bf83d045c8c1c9d
Anna <67124154+Anna-2901@users.noreply.github.com>
2020-09-25 16:41:16 +0300

    Добавляет решение дз№1

---
+++ js/task-2.js
@@ -0,0 +1,9 @@
+const total = 100;
+const ordered = 50;
+if (total < ordered) {
+  const message = `На складе недостаточно товаров!`;
+  console.log(message);
+} else {
+  const message = `Заказ оформлен, с вами свяжется менеджер`;
+  console.log(message);
+}
