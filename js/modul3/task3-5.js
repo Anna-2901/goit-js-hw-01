4708a789724fbc2b59f7026c4e495fbd061bd103
Anna <67124154+Anna-2901@users.noreply.github.com>
2020-10-10 19:44:23 +0300

    добавляет 4 модуль

--- js/modul3/task3-5.js
+++ js/modul3/task3-5.js
@@ -5,4 +5,33 @@
 
 Используй метод push для добавления значения в массив и оператор in для проверки наличия свойства в объекте.*/
 
-function getAllPropValues(array, prop) {}
+function getAllPropValues(array, prop) {
+  const staff = [];
+
+  for (const item of array) {
+    if (item[prop] != undefined) {
+      staff.push(item[prop]);
+    }
+    console.log(item[prop]);
+  }
+  return staff;
+}
+
+// Объекты и ожидаемый результат
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Радар", price: 1280, quantity: 2 },
+  { name: "Радар", price: 1320, quantity: 1 },
+  { name: "Сканер", price: 2700, quantity: 1 },
+  { name: "Сканер", price: 2500, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+console.log(getAllPropValues(products, "name"));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+console.log(getAllPropValues(products, "quantity"));
+// [4, 2, 1, 1, 3, 7, 2]
+
+console.log(getAllPropValues(products, "category"));
+//  []
